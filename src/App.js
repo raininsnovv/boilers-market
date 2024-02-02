c696e034f9cfa675b77e15a8f942252a23c026e4
raininsnovv <raininsnovv@gmail.com>
2024-02-02 16:54:02 +0300

    Начало верстки боковой корзины

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="cartItem d-flex align-center mt-20">
+            <img
+              className="mr-15"
+              width={110}
+              height={110}
+              src="/img/boilers/1.jpeg"
+              alt="Boilers"
+            />
+            <div className="mr-20 mb-15">
+              <p className="mb-5">Котел напольный Лемакс UNO 10 сталь</p>
+              <b>13 500 руб.</b>
+            </div>
+            <img
+              className="removeBtn mr-30"
+              src="/img/btn-remove.svg"
+              alt="Remove"
+            />
+          </div>
+          <div className="cartItem d-flex align-center mt-20">
+            <img
+              className="mr-15"
+              width={110}
+              height={110}
+              src="/img/boilers/2.jpeg"
+              alt="Boilers"
+            />
+            <div className="mr-20 mb-15">
+              <p className="mb-5">Котел напольный Лемакс UNO 10 сталь</p>
+              <b>13 500 руб.</b>
+            </div>
+            <img
+              className="removeBtn mr-30"
+              src="/img/btn-remove.svg"
+              alt="Remove"
+            />
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={50} height={50} src="/img/icon-boiler.png" alt="" />
@@ -25,16 +66,18 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <div>
-          <h1 className="mb-40">Все котлы</h1>
-          <div className="search-block">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все котлы</h1>
+          <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
             <input placeholder="Поиск..." />
           </div>
         </div>
         <div className="d-flex">
-          {" "}
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
             <img height={180} width={180} src="/img/boilers/1.jpeg" alt="" />
             <h5>Котел напольный Лемакс UNO 10 сталь</h5>
             <div className="d-flex justify-between align-center">
